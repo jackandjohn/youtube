a142caca439d1c24ee41adfa302a831466d43864
jackandjohn <99851762+jackandjohn@users.noreply.github.com>
2022-02-17 22:29:46 -0700

    Create spoiler.bookmarklet.js

---
+++ scripts/wordle/spoiler.bookmarklet.js
@@ -0,0 +1,10 @@
+javascript:
+(function(){
+  let url = 'https://www.nytimes.com/games/wordle/index.html';
+  let game = document.querySelector('game-app');
+  if(window.location.href === url) {
+    alert(game.solution);
+  } else {
+    window.location.href = url;
+  }
+}())
