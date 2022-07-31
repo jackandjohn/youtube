84058f1c8ab672a3a57bf8e21984c8507e4d7844
jackandjohn <99851762+jackandjohn@users.noreply.github.com>
2022-07-31 14:09:27 -0600

    revert to using local storage to get solution

--- scripts/wordle/spoiler.bookmarklet.js
+++ scripts/wordle/spoiler.bookmarklet.js
@@ -1,9 +1,9 @@
 javascript:
 (function(){
   let url = 'https://www.nytimes.com/games/wordle/index.html';
-  let game = document.querySelector('game-app');
+  let gameState = JSON.parse(localStorage['nyt-wordle-state']);
   if(window.location.href === url) {
-    alert(game.solution);
+    alert(gameState.solution);
   } else {
     window.location.href = url;
   }
